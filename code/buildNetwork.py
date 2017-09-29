93c3b2c9afc1266ac0cfd5a58b8fc6f1f7e20d02
Matt Brucker <mpbrucker@gmail.com>
2017-09-29 09:52:04 -0400

    Added new get_k function that appears to work.

--- code/buildNetwork.py
+++ code/buildNetwork.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 import networkx as nx
+from sympy import *
 from math import pi, sin
 
 
@@ -11,30 +12,49 @@ def get_k_min(mean_deg, gamma):
 
 def get_k_prob(val, mean_deg, gamma):
     min_k = get_k_min(mean_deg, gamma)
-    return (gamma - 1) * (min_k**(gamma - 1))*(val**(gamma*-1))
+    return (gamma - 1) * (min_k**(gamma - 1))*((val+1e-10)**(gamma*-1))
 
 
 def get_k_cdf(val, mean_deg, gamma):
     min_k = get_k_min(mean_deg, gamma)
-    return (-min_k**(gamma-1))*val**(1-gamma)-(-min_k**(gamma-1))*0.00001**(1-gamma)
+    k = Symbol('k', positive=True)
+    prob = get_k_prob(k, mean_deg, gamma)
+    cdf = integrate(prob, (k, 0, val))/integrate(prob, (k, 0, 1e64))
+    return cdf
+
+
+def get_k_cdf_norm(val, mean_deg, gamma):
+    return get_k_cdf(val, mean_deg, gamma)/get_k_cdf(1e10, mean_deg, gamma)
 
 
 def get_k_cdf_inv(val, mean_deg, gamma):
+    b = Symbol('b', positive=True)
+    y = Symbol('y', positive=True)
+    cdf=get_k_cdf(b,mean_deg,gamma)
+    soln=solve(y-cdf,b)[0]
+    print(soln.subs(y,val))
+
     min_k = get_k_min(mean_deg, gamma)
     a = (gamma - 1) * (min_k ** (gamma - 1))
-    d = (a*0.000001**(1-gamma))/(1-gamma)
+    d = (a*1e-10**(1-gamma))/(1-gamma)
     return (-(a+d)*(val**(1-gamma)))**(1/(1-gamma))
+
+
+def get_k(val, gamma, x0=1e-20, x1=1e20):
+    power = -gamma
+    return ((x1**(power+1) - x0**(power+1))*val + x0**(power+1))**(1/(power+1))
+
 
 
 def k_distribution(mean_deg, gamma):
     k_vals = np.linspace(0.000001, 1, 1e5)
-    k_probs = [get_k_cdf_inv(point, mean_deg, gamma) for point in k_vals]
+    k_probs = [get_k(point, gamma) for point in k_vals]
     return (k_vals, k_probs)
 
 
 def get_k_dist(mean_deg, gamma):
-    rand_inv_val = random.uniform(0, 68041381.74397714)
-    return get_k_cdf_inv(rand_inv_val, mean_deg, gamma)
+    rand_inv_val = random.uniform(0, 1)
+    return get_k(rand_inv_val, gamma)
 
 
 def all_possible_edges(G):
@@ -56,7 +76,8 @@ def connect_nodes(G, mean_deg, temp):
         d_val = (len(G.nodes())/(2*pi))*delta_theta
         mu = sin(temp*pi)/(2*mean_deg*temp*pi)
 
-        prob_connect = 1/(1+(d_val/(mu*ks[0]*ks[1])**(1/temp)))
+        prob_connect = 1/(1+((d_val/(mu*ks[0]*ks[1]))**(1/temp)))
+        print(d_val, mu, ks[0]*ks[1], prob_connect)
         if random.uniform(0, 1) < prob_connect:  # If we actually connect the nodes
             G.add_edge(*edge)
 
@@ -65,6 +86,7 @@ def build_network(n, gamma, mean_deg, temp):
     G = nx.Graph()
     thetas = [random.uniform(0, 2*pi) for _ in range(n)]  # Map every node to a theta from a uniform distribution
     k_vals = [get_k_dist(mean_deg, gamma) for _ in range(n)]  # Assign each node a k randomly
+    print(k_vals)
     all_nodes = range(n)
 
     thetas_dict = dict(zip(all_nodes, thetas))
@@ -80,9 +102,11 @@ def build_network(n, gamma, mean_deg, temp):
 
 
 # dist = k_distribution(5, 2.5)
-# print(get_k_dist(5, 2.5))
+print(new_get_k(.5,2.5))
 # plt.loglog(*dist)
 # plt.show()
 
-net = build_network(100, 2.5, 10, 0.5)
-nx.draw_circular(net)
+# net = build_network(10, 2.5, 0.1, 0.4)
+# print(nx.get_node_attributes(net, 'theta'))
+# nx.draw_circular(net)
+# plt.show()
