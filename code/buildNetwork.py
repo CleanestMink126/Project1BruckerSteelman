eee0d4df2d2e3bbd813eda911c47594533929e66
Matt Brucker <mpbrucker@gmail.com>
2017-09-29 10:43:44 -0400

    Added plotting functions.

--- code/buildNetwork.py
+++ code/buildNetwork.py
@@ -2,10 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 import networkx as nx
-from sympy import *
-from math import pi, sin
+from math import pi, sin, cos, log
 
-
+# Controls the value of the mean degree
 def get_k_min(mean_deg, gamma):
     return mean_deg*((gamma-2)/(gamma-1))
 
@@ -15,35 +14,13 @@ def get_k_prob(val, mean_deg, gamma):
     return (gamma - 1) * (min_k**(gamma - 1))*((val+1e-10)**(gamma*-1))
 
 
-def get_k_cdf(val, mean_deg, gamma):
-    min_k = get_k_min(mean_deg, gamma)
-    k = Symbol('k', positive=True)
-    prob = get_k_prob(k, mean_deg, gamma)
-    cdf = integrate(prob, (k, 0, val))/integrate(prob, (k, 0, 1e64))
-    return cdf
-
-
-def get_k_cdf_norm(val, mean_deg, gamma):
-    return get_k_cdf(val, mean_deg, gamma)/get_k_cdf(1e10, mean_deg, gamma)
-
-
-def get_k_cdf_inv(val, mean_deg, gamma):
-    b = Symbol('b', positive=True)
-    y = Symbol('y', positive=True)
-    cdf=get_k_cdf(b,mean_deg,gamma)
-    soln=solve(y-cdf,b)[0]
-    print(soln.subs(y,val))
-
-    min_k = get_k_min(mean_deg, gamma)
-    a = (gamma - 1) * (min_k ** (gamma - 1))
-    d = (a*1e-10**(1-gamma))/(1-gamma)
-    return (-(a+d)*(val**(1-gamma)))**(1/(1-gamma))
-
-
-def get_k(val, gamma, x0=1e-20, x1=1e20):
+# Doesn't appear to work correctly yet...
+def get_k(val, mean_deg, gamma, x0=1, x1=1e30):
     power = -gamma
-    return ((x1**(power+1) - x0**(power+1))*val + x0**(power+1))**(1/(power+1))
-
+    min_k = get_k_min(mean_deg, gamma)
+    C = (gamma-1)*min_k**(gamma-1)
+    # return (((power+1)/C)*val + x0**(power+1))**(1/(power+1))
+    return ((x1**(power+1) - x0**(power+1))*val + x0**(power+1))**(1/(power+1))*C
 
 
 def k_distribution(mean_deg, gamma):
@@ -52,9 +29,10 @@ def k_distribution(mean_deg, gamma):
     return (k_vals, k_probs)
 
 
+# Gets a value of K from the power-law distribution
 def get_k_dist(mean_deg, gamma):
     rand_inv_val = random.uniform(0, 1)
-    return get_k(rand_inv_val, gamma)
+    return get_k(rand_inv_val, mean_deg, gamma)
 
 
 def all_possible_edges(G):
@@ -77,16 +55,26 @@ def connect_nodes(G, mean_deg, temp):
         mu = sin(temp*pi)/(2*mean_deg*temp*pi)
 
         prob_connect = 1/(1+((d_val/(mu*ks[0]*ks[1]))**(1/temp)))
-        print(d_val, mu, ks[0]*ks[1], prob_connect)
         if random.uniform(0, 1) < prob_connect:  # If we actually connect the nodes
             G.add_edge(*edge)
+
+
+def set_r_vals(G, gamma, mean_deg, temp):
+    k_vals = nx.get_node_attributes(G, 'k')
+    c = mean_deg*(sin(temp*pi)/2*temp)*(((gamma-2)/(gamma-1))**2)
+    R = 2*log(len(G.nodes())/c)
+    k_min = get_k_min(mean_deg, gamma)
+    r_vals = [2*log(k_vals[node]/k_min) for node in G.nodes()]
+    r_dict = dict(zip(G.nodes(), r_vals))
+    nx.set_node_attributes(G, 'r', r_dict)
+    return r_dict
 
 
 def build_network(n, gamma, mean_deg, temp):
     G = nx.Graph()
     thetas = [random.uniform(0, 2*pi) for _ in range(n)]  # Map every node to a theta from a uniform distribution
     k_vals = [get_k_dist(mean_deg, gamma) for _ in range(n)]  # Assign each node a k randomly
-    print(k_vals)
+    # print(k_vals)
     all_nodes = range(n)
 
     thetas_dict = dict(zip(all_nodes, thetas))
@@ -97,16 +85,21 @@ def build_network(n, gamma, mean_deg, temp):
     nx.set_node_attributes(G, 'k', k_dict)
 
     connect_nodes(G, mean_deg, temp)
+    r_vals = set_r_vals(G, gamma, mean_deg, temp)
+
+    pos_vals = [(r_vals[node]*cos(thetas_dict[node]), r_vals[node]*sin(thetas_dict[node])) for node in all_nodes]
+    pos_dict = dict(zip(all_nodes, pos_vals))
+    nx.set_node_attributes(G, 'pos', pos_dict)
 
     return G
 
 
-# dist = k_distribution(5, 2.5)
-print(new_get_k(.5,2.5))
-# plt.loglog(*dist)
-# plt.show()
+def draw_net(graph, **kwargs):
+    print(kwargs)
+    nx.draw(graph, nx.get_node_attributes(graph, 'pos'), **kwargs)
 
-# net = build_network(10, 2.5, 0.1, 0.4)
-# print(nx.get_node_attributes(net, 'theta'))
-# nx.draw_circular(net)
-# plt.show()
+
+net = build_network(1000, 2.5, 3, 0.4)
+# print(nx.get_node_attributes(net, 'r'))
+draw_net(net, node_size=20, node_color='orange', width=0.1)
+plt.show()
