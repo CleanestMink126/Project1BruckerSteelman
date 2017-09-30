bdbe42a0a7fff20215872df4655f1b209a78813c
CleanestMink126 <steelman@students.olin.edu>
2017-09-30 15:59:42 -0400

    exaining use w actuall graph

--- code/transverseGraph.py
+++ code/transverseGraph.py
@@ -3,7 +3,10 @@ import networkx as nx
 import math
 from buildNetwork import build_network
 
+
+attributes = ['pos','weight','defector','theta','k']
 class graphCrawler:
+
     def __init__(self,G,b,posDict=None,weightDict = None,defectorDict = None):
         self.G = G #the graph
         self.b = b #the payoff
@@ -30,13 +33,15 @@ class graphCrawler:
     def findClosestNode(self, node, target):
         allNeighbors = self.G.neighbors(node)#get all neighbors of the node
         allNeighbors = allNeighbors.copy()
+        print(nx.get_node_attributes(self.G, 'pos'))
         # print(allNeighbors)
-        closeNode = allNeighbors.popitem()[0]#get last value and set to closest
-        while closeNode in ['pos','weight','defector']:
-            closeNode = allNeighbors.popitem()[0]
+        closeNode = allNeighbors.pop()#get last value and set to closest
+        while closeNode in attributes:
+            closeNode = allNeighbors.pop()
+        print(self.G[closeNode])
         minDist = self.findDistance(closeNode,target)#set last value distance to min
-        for i,v in allNeighbors.items():#loop to check distance of all neighbors
-            if i not in ['pos','weight','defector']:
+        for i in allNeighbors:#loop to check distance of all neighbors
+            if i not in attributes:
                 dist = self.findDistance(i,target)
                 if dist < minDist:
                     misDist = dist
@@ -62,13 +67,23 @@ class graphCrawler:
 
     def updateConversion(self):
         for node in G.nodes():
-            allNeighbors = self.G[node]#get all neighbors of the node
-            closeNode = random.choice(allNeighbors.keys())#random node
-            probabilityChange = 1 / (1 + math.exp((self.G[node]['weight']-self.G[closenode]['weight'])/self.k))
+            allNeighbors = self.G.neighbors(node)#get all neighbors of the node
+            allNeighbors = allNeighbors.copy()
+            # print(allNeighbors)
+            closeNode =random.choice(allNeighbors)#get last value and set to closest
+            while closeNode in attributes:
+                closeNode = random.choice(allNeighbors)
+            weightj = self.G[closeNode]['weight']
+            weighti = self.G[node]['weight']
+            print(weighti)
+            print(weightj)
+            expVal = math.exp((weighti-weightj)/self.k)
+            print(expVal)
+            probabilityChange = 1 / (1 + expVal)
                                      #calculate proability that the original node will copy its neighbor
             changeBool = random.random() < probabilityChange#make decision based on probability
             if changeBool:
-                self.defectorDict[node] = self.G[closenode]['defector']#store difference in defectorness in another array
+                self.defectorDict[node] = self.G[closeNode]['defector']#store difference in defectorness in another array
         for node in G.nodes():#update and reset weights
             self.G[i]['weight'] = self.weightDict[i]
             self.G[i]['defector'] = self.defectorDict[i]
@@ -76,7 +91,7 @@ class graphCrawler:
     def iterate(self, numTimes):
         for i in range(numTimes):
             for i in range(nx.number_of_nodes(self.G)):
-                nodeList = random.sample(G.nodes(), 2)
+                nodeList = random.sample(self.G.nodes(), 2)
                 node1 = nodeList[0]
                 node2 = nodeList[1]
                 path = self.getPath(node1,node2)
@@ -86,16 +101,24 @@ class graphCrawler:
                     self.updateWeights(path,0)
             self.updateConversion()
 
-n = 100
-k = 10
-C = .6
-G = nx.generators.random_graphs.powerlaw_cluster_graph(n, k, C, seed=None)
-posDict = {}
-for i in G.nodes():
-    posDict[i] = (random.randint(0,100),random.randint(0,100))
-# print(posDict)
-myCrawler = graphCrawler(G,10,posDict)
-myCrawler.iterate(10)
+if __name__ == '__main__':
+    n = 100
+    gamma = 2.5
+    temp = 0.4
+    meanDeg = 6
+    graph = build_network(n = n, gamma = gamma, temp = temp, mean_deg = meanDeg)
+    myCrawler = graphCrawler(graph,5)
+    myCrawler.iterate(10)
+# n = 100
+# k = 10
+# C = .6
+# G = nx.generators.random_graphs.powerlaw_cluster_graph(n, k, C, seed=None)
+# posDict = {}
+# for i in G.nodes():
+#     posDict[i] = (random.randint(0,100),random.randint(0,100))
+# # print(posDict)
+# myCrawler = graphCrawler(G,10,posDict)
+# myCrawler.iterate(10)
 # pos = dict(Albany=(-74, 43),
 #           Boston=(-71, 42),
 #           NYC=(-74, 41),
